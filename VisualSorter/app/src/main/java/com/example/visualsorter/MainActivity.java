5387c08ffc0cfdb657207c9e938fdb511796db60
Skipping903 <albieluk000@yahoo.com>
2019-04-09 10:50:15 -0500

    Added comments
    Added comments to show understanding (hopefully)

--- VisualSorter/app/src/main/java/com/example/visualsorter/MainActivity.java
+++ VisualSorter/app/src/main/java/com/example/visualsorter/MainActivity.java
@@ -56,35 +56,49 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /*
+     This was my third attempt to create the code, with inspiration taken from
+     an online source so that I could understand a working version.
+     I could understand how it works, but I couldn't come up with code without reference.
+    */
     public static void mergeSort(int[] ar, int arLength)
     {
         if (arLength < 2)
         {
             return;
         }
-        int mid = arLength / 2;
+        //Checks the number of elements in the array, if only one element then return
+
+        int mid = arLength / 2;//An int that holds the middle bucket number of ar
         int[] left = new int[mid];
         int[] right = new int[arLength - mid];
+        //Creates two arrays meant to hold contents of ar, split down the middle
 
         for (int start1 = 0; start1 < mid; start1++)
         {
             left[start1] = ar[start1];
-        }
+        }//This loop adds contents to the left array up until the middle of ar
 
         for (int start2 = mid; start2 < arLength; start2++)
         {
             right[start2 - mid] = ar[start2];
-        }
+        }//This loop adds contents to the right array starting from the middle of ar to its end
+
         mergeSort(left, mid);
         mergeSort(right, arLength - mid);
+        //Calls mergesort on left and right, creating new left and right arrays and
+        // splitting them at their middle until both arrays only contain one element
+
         merge(ar, left, right, mid, arLength - mid);
+        //Merge is called to take all of the split lists and combine them back into ar
     }
 
     public static void merge(int[] ar, int[] left, int[] right, int leftPos, int rightPos)
     {
         int currLeft = 0, currMid = 0, currRight = 0;
+        //Counters to keep track of what bucket in the split lists the loops are in
 
-        while (currLeft < leftPos && currMid < rightPos)
+        while (currLeft < leftPos && currMid < rightPos)//leftPos and rightPos tell the loops how far into ar to add integers
         {
             if (left[currLeft] <= right[currMid])
             {
@@ -94,15 +108,22 @@ public class MainActivity extends AppCompatActivity
             {
                 ar[currRight++] = right[currMid++];
             }
+            //Checks whether the int in the current position of left is smaller than the int
+            // in the current position of right; if it is, then add left's int back to ar,
+            // otherwise add right's integer back
         }
         while (currLeft < leftPos)
         {
             ar[currRight++] = left[currLeft++];
         }
+
         while (currMid < rightPos)
         {
             ar[currRight++] = right[currMid++];
         }
+        //Takes the smaller remaining values from the left and right arrays and puts them back into ar
+        //Due to how the integers in the previous loop was sorted, and how the ints were split up into smaller lists
+        //by calling mergeSort multiple times, the integers fall into the correct order at this point of the code.
     }
 
     //recursion refresher example
